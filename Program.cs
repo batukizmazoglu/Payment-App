d5fd47a86b0bd2269c35a504ec0661bfedf245c6
Batu Kızmazoğlu <106972755+batukizmazoglu@users.noreply.github.com>
2024-07-29 09:40:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+using Payment.Web.Data;
+using Payment.Web.Pages;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MudBlazor.Services;
+using Payment.Client;
+using Payment.Shared.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+//builder.Services.AddSingleton<WeatherForecastService>();
+//builder.Services.AddScoped(x=> new TestModel(1));
+//builder.Services.tra<List<dataGridCustomers>>();
+
+//builder.Services.AddDbContext<TestContext>(options =>
+//             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<CustomerService>();
+
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+
+
+app.Run();
